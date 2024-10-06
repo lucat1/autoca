f6d4d1b3e4caddad7856c95d5263ef3c4a40c392
Luca Tagliavini <luca@teapot.ovh>
2024-10-06 11:10:27 +0000

    Configure for packaging

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,6 @@
+Bug-Database: https://github.com/lucat1/autoca/issues
+Bug-Submit: https://github.com/lucat1/autoca/issues/new
+Changelog: https://github.com/lucat1/autoca/blob/master/CHANGES
+Documentation: https://github.com/lucat1/autoca/wiki
+Repository-Browse: https://github.com/lucat1/autoca
+Repository: https://github.com/lucat1/autoca.git
